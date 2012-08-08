852bbe8ae04f110d4b07cab0eefc0280dbf30f4b
Roey Darwish Dror <roey.ghost@gmail.com>
2012-08-08 21:05:02 +0300

    Removed the documentation of zap-up-to-char

--- init.el
+++ init.el
@@ -73,9 +73,6 @@
 
 ; zap-up-to-char
 (autoload 'zap-up-to-char "misc"
-  "Kill up to, but not including ARGth occurrence of CHAR.
-  
-  \(fn arg char)"
   'interactive)
 (global-set-key (kbd "M-z") 'zap-up-to-char)
 
