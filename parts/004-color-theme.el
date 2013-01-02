00d953299ad0a30914301d39887084817ef704bb
Roey Darwish Dror <roey.ghost@gmail.com>
2013-01-02 21:21:38 +0200

    Added base16 theme

--- parts/004-color-theme.el
+++ parts/004-color-theme.el
@@ -1,7 +1,8 @@
 ; Color theme
 (cond
  ((>= emacs-major-version 24)
-  (add-to-list 'custom-theme-load-path (in-emacs-d "themes")))
+  (add-to-list 'custom-theme-load-path (in-emacs-d "themes"))
+  (add-to-list 'custom-theme-load-path (in-emacs-d "themes/base16")))
  ((< emacs-major-version 24)
   (add-to-list 'load-path (in-emacs-d "legacy/themes/"))
   (load-library "color-theme")
