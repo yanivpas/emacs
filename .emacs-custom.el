288f1383425818014c4ed92da751f2d9ade2fda8
Roey Darwish Dror <roey.ghost@gmail.com>
2013-01-11 12:16:21 +0200

    Removed font definition from emacs custom

--- .emacs-custom.el
+++ .emacs-custom.el
@@ -11,7 +11,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#000000" :foreground "#dedede" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "apple" :family "Monaco"))))
  '(ace-jump-face-foreground ((t (:foreground "yellow" :weight bold))))
  '(highlight ((t (:background "Yellow" :foreground "black"))))
  '(hl-line ((t (:background "#333"))) t)
