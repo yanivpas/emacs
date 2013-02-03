c3f288710d2390e3ca1b37da1c006c289728fd68
Roey Darwish Dror <roey.ghost@gmail.com>
2013-02-03 10:12:34 +0200

    Removed battery mode from emacs custom

--- .emacs-custom.el
+++ .emacs-custom.el
@@ -6,7 +6,6 @@
  '(cua-mode t nil (cua-base))
  '(cua-rectangle-mark-key (kbd "C-x <C-return>"))
  '(custom-safe-themes (quote ("fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "545287ef14b1b686cd88a99cf6ab3629e55ef6b73d59a76253eec65f4ead4fee" "4d66773cc6d32566eaf2c9c7ce11269d9eb26e428a1a4fa10e97bae46ff615da" default)))
- '(display-battery-mode t)
  '(display-time-mode t)
  '(long-line-limit 100)
  '(minimap-window-location (quote right))
